2ed6fe1b9ef4df7a5fd4e2ac3d201e3b1083cce7
Jordan Davila Terrones <jjordandt@gmail.com>
2023-10-16 23:09:10 -0500

    Domain - Evetn, command

---
+++ muchik.market.domain/bus/IEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using muchik.market.domain.events;
+
+namespace muchik.market.domain.bus
+{
+    internal interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler { }
+}
