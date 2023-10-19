da73401830c0b6ffaef9281a0d90117f5c5af9fd
Jordan Davila Terrones <jjordandt@gmail.com>
2023-10-18 22:36:40 -0500

    infrastructure crosscutting

---
+++ muchik.market.infrastructure.crosscutting/Jwt/IJwtManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace muchik.market.infrastructure.crosscutting.Jwt
+{
+    internal interface IJwtManager
+    {
+        string GenerateToken(string userId, string username, string customerId, string role);
+    }
+}
---
+++ muchik.market.infrastructure.crosscutting/Models/ExceptionResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace muchik.market.infrastructure.crosscutting.Models
+{
+    internal class ExceptionResponseModel
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; } = null!;
+        public string StackTrace { get; set; } = null!;
+    }
+}
